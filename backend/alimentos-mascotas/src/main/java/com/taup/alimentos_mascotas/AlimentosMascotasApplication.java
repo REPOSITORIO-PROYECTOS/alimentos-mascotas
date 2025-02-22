3b8e4b8572f664bbb6805af4892baa1e0ca2be49
NicoDev92 <alenolmedo92@gmail.com>
2025-02-22 19:13:02 -0300

    Inicializacion del proyecto

---
+++ backend/alimentos-mascotas/src/main/java/com/taup/alimentos_mascotas/AlimentosMascotasApplication.java
@@ -0,0 +1,13 @@
+package com.taup.alimentos_mascotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlimentosMascotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlimentosMascotasApplication.class, args);
+	}
+
+}
---
+++ backend/alimentos-mascotas/src/test/java/com/taup/alimentos_mascotas/AlimentosMascotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taup.alimentos_mascotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlimentosMascotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
