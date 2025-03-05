45583a697b085db4a1d07e0fa9488d7ef5d46504
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2025-03-04 23:09:27 -0300

    Feat: Agregar componente Hero que incluye SwiperHome para presentación de imágenes

---
+++ frontend/src/components/interfaces/Hero.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SwiperHome from '../swiperHome'
+
+export default function Hero() {
+    return (
+        <section className="h-auto w-full mt-16">
+            <SwiperHome />
+        </section>
+    )
+}
