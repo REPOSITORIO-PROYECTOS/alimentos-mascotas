45cd5f28e9af573324c24d441de350e01d273a93
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2025-03-04 23:08:54 -0300

    Feat: Agregar páginas de administración para control de asistencia y paneles de cursos, inventario y usuarios

---
+++ frontend/src/app/admin/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "@/styles/globals.css";
+import { Navbar } from "@/components/navbar";
+import { Toaster } from "sonner";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+    title: "Sistema gestion",
+    description: "Sistema de gestion de ventas",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="es">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
+            >
+                <Navbar />
+                <main className="flex-grow">
+                    {children}
+                    <Toaster />
+                </main>
+                {/* <Footer /> */}
+            </body>
+        </html>
+    );
+}
