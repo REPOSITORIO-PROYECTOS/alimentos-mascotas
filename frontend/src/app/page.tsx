7f9e4d6e95f3af3758af870bde8859d99fc86dc7
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2025-05-07 00:32:16 -0300

    feat: reorganizar componentes en la página principal y eliminar elementos comentados

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,19 @@
+import Carrusel from "@/components/interfaces/carrusel";
 import Cta from "@/components/interfaces/Cta";
 import Hero from "@/components/interfaces/Hero";
 import Products from "@/components/interfaces/Products";
+import Secciones from "@/components/interfaces/secciones";
+import TablaDiaria from "@/components/interfaces/tabla-diaria";
 
 export default function Home() {
     return (
         <main>
             <Hero />
-            <Products />
-            <Cta />
+            <Carrusel />
+            <Secciones />
+            {/* <Products /> */}
+            <TablaDiaria />
+            {/* <Cta /> */}
         </main>
     );
 }
