2e3e4a5c4cfbe1f5cee3dd3b007d75f59c782ccb
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2025-05-18 03:41:17 -0300

    feat: corregir texto en el carrusel y reemplazar enlace por componente Link

--- frontend/src/components/interfaces/carrusel.tsx
+++ frontend/src/components/interfaces/carrusel.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { AutoSlideGallery } from "../auto-slide-gallery";
+import Link from "next/link";
 
 export default function Carrusel() {
     return (
@@ -7,7 +8,7 @@ export default function Carrusel() {
             <div className="container mx-auto py-12">
                 <header className="relative z-10 flex flex-col items-center justify-center gap-6 bg-background text-center">
                     <h2 className="text-2xl font-bold font-sans tracking-widest text-primary uppercase lg:text-3xl">
-                        Recomendandos con amor
+                        Recomendados con amor
                         <span className="text-primary">.</span>
                     </h2>
                     <p className="mt-2 mb-4 max-w-xl text-lg text-muted-foreground md:text-xl">
@@ -21,8 +22,8 @@ export default function Carrusel() {
                 <AutoSlideGallery />
             </div>
             <div className="container mx-auto flex items-center justify-center py-8">
-                <a
-                    href="#"
+                <Link
+                    href="/products"
                     data-slot="button"
                     className="items-center justify-center gap-2 whitespace-nowrap transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([className*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive bg-primary text-primary-foreground shadow-xs hover:bg-primary/90 has-[>svg]:px-3 group block h-fit w-fit rounded-full px-7 py-6 text-base leading-[1] font-bold"
                 >
@@ -32,7 +33,7 @@ export default function Carrusel() {
                     >
                         Ver mas productos
                     </span>
-                </a>
+                </Link>
             </div>
         </section>
     );
