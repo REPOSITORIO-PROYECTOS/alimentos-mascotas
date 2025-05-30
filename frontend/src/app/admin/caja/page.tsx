582e97a316bc242ba04716b58260a45700e4988e
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2025-05-29 21:25:45 -0300

    feat: agrega componente de página para el panel de caja

---
+++ frontend/src/app/admin/caja/page.tsx
@@ -0,0 +1,14 @@
+import TableCashRegister from "@/components/interfaces/table-cash-register";
+
+export default function Page() {
+    return (
+        <>
+            <div className="container flex flex-col gap-12 mx-auto p-16">
+                <h2 className="text-[#1e1e1e] text-2xl font-semibold">
+                    PANEL DE CAJA
+                </h2>
+            </div>
+            <TableCashRegister />
+        </>
+    );
+}
