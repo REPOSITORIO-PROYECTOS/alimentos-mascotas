35c56599ec272753e67e040c5847bba1990ba12f
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2025-06-02 20:59:16 -0300

    feat: agrega función toggleCajaEstado para gestionar el estado de la caja

---
+++ frontend/src/actions/caja-actions.ts
@@ -0,0 +1,38 @@
+"use server";
+
+export async function toggleCajaEstado(activa: boolean, token: string) {
+    try {
+        const response = await fetch(
+            `https://barker.sistemataup.online/api/caja/estado?operacion=${
+                activa ? "abrir" : "cerrar"
+            }`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+                body: JSON.stringify({
+                    operacion: `${activa ? "abrir" : "cerrar"}`,
+                }),
+                // Importante: agregar esta opción si la API está en otro dominio
+                //cache: "no-store",
+            }
+        );
+
+        const data = await response.json();
+        console.log("Respuesta de la API:", data);
+
+        if (!response.ok) {
+            throw new Error("Error al cambiar el estado de la caja");
+        }
+
+        return { success: true, data };
+    } catch (error) {
+        console.error("Error en server action:", error);
+        return {
+            success: false,
+            error: "Error al cambiar el estado de la caja",
+        };
+    }
+}
